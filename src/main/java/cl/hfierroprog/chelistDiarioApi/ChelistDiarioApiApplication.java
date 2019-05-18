3f41a717072d93c64bc1b7c51fcdedb22b3f89cd
Hector <hfierroprog@gmail.com>
2019-05-18 01:39:51 -0400

    Se agrega metodo health

--- src/main/java/cl/hfierroprog/chelistDiarioApi/ChelistDiarioApiApplication.java
+++ src/main/java/cl/hfierroprog/chelistDiarioApi/ChelistDiarioApiApplication.java
@@ -3,12 +3,21 @@ package cl.hfierroprog.chelistDiarioApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ChelistDiarioApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChelistDiarioApiApplication.class, args);
 	}
 
+	@GetMapping("/health")
+	public ResponseEntity<Boolean> health() {
+		return new ResponseEntity<>(Boolean.TRUE, HttpStatus.OK);
+	}
 }
