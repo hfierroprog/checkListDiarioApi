afe889518e1692736fb89d33aca53e6676167624
Hector <hfierroprog@gmail.com>
2019-05-18 01:23:05 -0400

    Integracion continua tomcat

--- src/main/java/cl/hfierroprog/chelistDiarioApi/ChelistDiarioApiApplication.java
+++ src/main/java/cl/hfierroprog/chelistDiarioApi/ChelistDiarioApiApplication.java
@@ -2,9 +2,10 @@ package cl.hfierroprog.chelistDiarioApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChelistDiarioApiApplication {
+public class ChelistDiarioApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChelistDiarioApiApplication.class, args);
