f7b339a211a90f94e23bfc04e9ccfad2fc7426df
Hector <hfierroprog@gmail.com>
2019-05-18 21:50:41 -0400

    Crear la base de datos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO `pool` VALUES (1,'Levantarme al sonar la alarma'),(2,'Tomar agua al despertar'),(3,'Hacer la cama en la mañana'),(4,'Estirar y luego hacer ejercicio'),(5,'Ducha con agua fría');
